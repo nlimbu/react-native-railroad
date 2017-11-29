bfe1cc76ae94c8df3476738dff0d1124239c3e14
softCompound <nl239@kent.ac.uk>
2017-11-29 00:20:39 +0000

    Modified readme.md

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ import RailRoad from 'react-native-railroad';
 
 <RailRoad 
     index={2}
-    max_index={5}
-    style={style}
+    max_index={4}
+    color="#e6e600"
 />
 ```
 
@@ -25,8 +25,8 @@ import RailRoad from 'react-native-railroad';
 
 Props | Default Values | Behaviour
 ----- | -------------- | --------
-index|2| 
-max_index|5| Total units in the railroad.
-percent|0|Takes value from 0 - 1 and times with the width of the component to display the meter progress.
+max_index|4| Total units in the railroad.
+color|"#e6e600"| The color to be applied mark the current state in the railroad. 
+index|2|It marks the stages in the railroad. For a value of index=2 on a max_index value=4, the specified color will be applied to mark the two out of the max four units of the railroad.
 
 
--- index.js
+++ index.js
@@ -8,19 +8,19 @@ export default class RailRoad extends Component {
     }
 
     render() {
-        const { max_index, index, style } = this.props;
+        const { max_index, index, color } = this.props;
         const width = (Dimensions.get('window').width / max_index) - 12;
 
         let rail_styles = [];
         for (let i = 0; i < max_index; i++) {
-            const color = index > i ? '#e6e600' : '#c2c2a3';
+            const backgroundColor = color ? index > i ? color : '#c2c2a3' : index > i ? '#e6e600' : '#c2c2a3';
 
             rail_styles.push({
                 alignSelf: 'center',
                 borderRadius: 1,
                 height: 4,
                 marginRight: 2,
-                backgroundColor: color,
+                backgroundColor,
                 width: width
             });
         }
@@ -28,9 +28,9 @@ export default class RailRoad extends Component {
         return (
             <View style={styles.main} >
                 {
-                    rail_styles.map((rail_Style, i) => {
+                    rail_styles.map((style, i) => {
                         return (
-                            <Animated.View key={i} style={[rail_Style, style]} />
+                            <Animated.View key={i} style={style} />
                         )
                     })
                 }
