4191791f275390d30d4a09fc78091a7a2f8c5eea
softCompound <nl239@kent.ac.uk>
2017-12-06 10:07:21 +0000

    Updated Railroad Component usability with additional props

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ import React, { Component } from 'react';
 import { View, Animated, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 
+const GREY = '#adad85';
+const OPAQUE = 'rgba(0,0,0,0)';
+
 export const RailRoad = (props) => {
-    const { max_index, index, color } = props;
-    const width = (Dimensions.get('window').width / max_index) - 12;
+    const default_width = (Dimensions.get('window').width / props.max_index) - 12;
+    const { max_index, index, color = '#e6e600', shape, mode, width = default_width, height = 4, circleSize, emptyFill } = props;
+    const shape_style =
+        shape === 'line' ?
+            { width, height, marginHorizontal: 1 } :
+            shape === 'circle' ?
+                { borderRadius: circleSize / 2, width: circleSize, height: circleSize, marginHorizontal: circleSize / 2 } :
+                0;
 
     let rail_styles = [];
-    for (let i = 0; i < max_index; i++) {
-        const backgroundColor = color ? index > i ? color : '#c2c2a3' : index > i ? '#e6e600' : '#c2c2a3';
+    for (let i = 1; i <= max_index; i++) {
+        const style = mode === 'inclusive' ?
+            index >= i ?
+                { ...shape_style, backgroundColor: color } :
+                emptyFill ? { ...shape_style, backgroundColor: emptyFill } : { ...shape_style, backgroundColor: OPAQUE, borderWidth: 1, borderColor: GREY } :
+            index === i ?
+                { ...shape_style, backgroundColor: color } :
+                emptyFill ? { ...shape_style, backgroundColor: emptyFill } : { ...shape_style, backgroundColor: OPAQUE, borderWidth: 1, borderColor: GREY };
 
         rail_styles.push({
             alignSelf: 'center',
-            borderRadius: 1,
-            height: 4,
-            marginRight: 2,
-            backgroundColor,
-            width: width
+            ...style
         });
     }
 
@@ -36,7 +47,18 @@ export const RailRoad = (props) => {
 RailRoad.propTypes = {
     max_index: PropTypes.number.isRequired,
     index: PropTypes.number.isRequired,
-    color: PropTypes.string
+    emptyFill: PropTypes.string,
+    color: PropTypes.string,
+    shape: PropTypes.oneOf(['line', 'circle']).isRequired,
+    mode: PropTypes.oneOf(['inclusive', 'exclusive']).isRequired,
+    circleSize: function (props, propName, componentName) {
+        if (props['shape'] == 'circle' && !props['circleSize']) {
+            return new Error(
+                'Invalid prop `' + propName + '` supplied to' +
+                ' `' + componentName + '`. Validation failed.'
+            );
+        }
+    }
 };
 
 const styles = {
